f942c7da1367a330468f7556066967c3f1a608e3
Gnudoc <20248043+gnudoc@users.noreply.github.com>
2023-06-20 20:03:04 +0100

    auto-added custom-set-variables and custom-set-faces

--- init.el
+++ init.el
@@ -112,3 +112,16 @@
 ;;(use-package forge) ;some sqlite3 issue with this 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit counsel-projectile projectile helpful ivy-rich counsel which-key doom-modeline all-the-icons doom-themes ivy rainbow-delimiters use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
