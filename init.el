afa999de0de7c4b10fcc70faf5c1a998fd15061a
Gnudoc <20248043+gnudoc@users.noreply.github.com>
2023-06-20 19:51:28 +0100

    removed forge and the auto-added bits

--- init.el
+++ init.el
@@ -109,19 +109,6 @@
 ;;:custom
 ;;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)) ; if you dont like diff in a separate buffer
 
-(use-package forge)
+;;(use-package forge) ;some sqlite3 issue with this 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(sqlite3 forge magit counsel-projectile projectile all-the-icons doom-themes ivy-rich rainbow-delimiters use-package ivy doom-modeline)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
