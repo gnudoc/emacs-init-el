d8a5c534631039f8940f6c7e73484b4abad2a2fe
Gnudoc <gnudoc.social+github@gmail.com>
2023-07-10 13:22:57 +0100

    Rejigged the setup to have Emacs.org creating init.el

--- init.el
+++ init.el
@@ -1,42 +1,4 @@
-;; Basic UI Configuration ------------------------------------------------------
-
-;; You will most likely need to adjust this font size for your system!
-(defvar nij/default-font-size 100)
-
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-
-;;(menu-bar-mode -1)            ; Disable the menu bar once you're confident without it
-
-;; Set up the visible bell
-(setq visible-bell t)
-
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(column-number-mode)
-(global-display-line-numbers-mode t)
-
-;; Font Configuration ----------------------------------------------------------
-
-(set-face-attribute 'default nil :font "Fira Code Retina" :height nij/default-font-size)
-;;(set-face-attribute 'default nil :font "FiraCode Nerd Font") another slightly lighter-weight version
-
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 130)
-
-;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 140 :weight 'regular)
-
-;; Package Manager Configuration -----------------------------------------------
+;; Note that init.el is generated from ./Emacs.org - that is the file that should be editted.
 
 ;; Initialize package sources
 (require 'package)
@@ -56,7 +18,55 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Ivy and related Configuration -----------------------------------------------------------
+;; You will most likely need to adjust these font sizes for your system
+(defvar nij/default-font-size 130)
+(defvar nij/default-variable-font-size 140)
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)            ; Disable the menu bar - comment this if you're not confident with keybindings yet
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height nij/default-font-size)
+;;(set-face-attribute 'default nil :font "FiraCode Nerd Font") another slightly lighter-weight version
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 130)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 140 :weight 'regular)
+
+(use-package doom-themes
+  :init (load-theme 'doom-dracula t))
+
+(use-package all-the-icons)
+(use-package nerd-icons)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.5))
 
 (use-package ivy
   :diminish
@@ -76,31 +86,6 @@
   :config
   (ivy-mode 1))
 
-;; NOTE: The first time you load your configuration on a new machine, you'll
-;; need to run the following command interactively so that mode line icons
-;; display correctly:
-;;
-;; M-x all-the-icons-install-fonts
-
-(use-package all-the-icons)
-(use-package nerd-icons)
-
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
-
-(use-package doom-themes
-  :init (load-theme 'doom-dracula t))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.5))
-
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
@@ -110,7 +95,8 @@
          ("C-x b" . counsel-ibuffer)
          ("C-x C-f" . counsel-find-file)
          :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
+         ("C-r" . 'counsel-minibuffer-history))
+  :config (counsel-mode 1))
 
 (use-package helpful
   :custom
@@ -121,41 +107,6 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
-
-;; Projectile Configuration ----------------------------------------------------
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder containing Git and other code repos!
-  (when (file-directory-p "~/Projects/Code")
-    (setq projectile-project-search-path '("~/Projects/Code")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-;; Magit Configuration ---------------------------------------------------------
-
-(use-package magit
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-;; NOTE: Make sure to configure a GitHub token before using this package!
-;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
-;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
-(use-package forge)
-
-;; Org Mode Configuration ------------------------------------------------------
-
-(defun nij/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
 
 (defun nij/org-font-setup ()
   ;; Replace list hyphen with dot
@@ -183,12 +134,25 @@
   (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
   (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
+(defun nij/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+
 (use-package org
   :hook (org-mode . nij/org-mode-setup)
   :config
   (setq org-ellipsis " ▾")
-  (setq org-agenda-files
-	'("~/Documents/OrgFiles/Tasks.org"))
+;;  (setq org-agenda-start-with-log-mode t)
+;;  (setq org-log-done 'time)
+;;  (setq org-log-into-drawer t)
+;;  (setq org-agenda-files
+;;	'("~/Documents/OrgFiles/Tasks.org"))
+;;  (require 'org-habit)
+;;  (add-to-list 'org-modules 'org-habit)
+;;  (setq org-habit-graph-column 60) ;; this might need amended
+;;;; there's tons more that can be put in here - look at daviwil's emacs-from-scratch emacs.org org mode section for example
   (nij/org-font-setup))
 
 (use-package org-bullets
@@ -198,24 +162,114 @@
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (defun nij/org-mode-visual-fill ()
-  (setq visual-fill-column-width 180
+  (setq visual-fill-column-width 250
         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
   :hook (org-mode . nij/org-mode-visual-fill))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
- '(package-selected-packages
-   '(visual-fill-column org-bullets forge magit counsel-projectile projectile helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline nerd-icons all-the-icons ivy use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((emacs-lisp . t)
+    (python . t)))
+
+(push '("conf-unix" . conf-unix) org-src-lang-modes)
+
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+
+;; Automatically tange our Emacs.org config file when we save it
+(defun nij/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.emacs.d/Emacs.org"))
+    ;; Dynamic scoping example
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'nij/org-babel-tangle-config)))
+
+(defun nij/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook ((lsp-mode . nij/lsp-mode-setup)
+         (cc-mode . lsp-deferred)) ;; for c and c++
+  :init (setq lsp-keymap-prefix "C-c l")
+  :config (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+:hook (lsp-mode . lsp-ui-mode)
+:custom (lsp-ui-doc-position 'bottom))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy)
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder containing Git and other code repos!
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;; the first time you run emacs with this config it'll complain that some function definition is void or something like that.
+;; you have to (unload-feature sqlite3) then (require 'sqlite3) again.
+(use-package sqlite3)
+
+(require 'sqlite3)
+
+(setq dbh (sqlite3-open "person.sqlite3" sqlite-open-readwrite sqlite-open-create))
+(sqlite3-exec dbh "create table temp (name text, age integer)")
+(setq stmt (sqlite3-prepare dbh "insert into temp values (?,?)"))
+(cl-loop for i from 1 to 10 do
+         (sqlite3-bind-multi stmt (format "name%d" i) i)
+          ;; execute the SQL
+         (sqlite3-step stmt)
+         ;; call reset if you want to bind the SQL to a new set of variables
+         (sqlite3-reset stmt))
+(sqlite3-finalize stmt)
+
+(setq stmt (sqlite3-prepare dbh "select * from temp"))
+(while (= sqlite-row (sqlite3-step stmt))
+  (cl-destructuring-bind (name age) (sqlite3-fetch stmt)
+    (message "name: %s, age: %d" name age)))
+(sqlite3-finalize stmt)
+(sqlite3-close dbh)
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+(use-package forge)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
