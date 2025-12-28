b9050f9686e7a11830b9aea27507d99481a86215
Aijaz Mohammad <20248043+gnudoc@users.noreply.github.com>
2025-12-28 08:25:27 +0000

    First attempt to try and make vterm talk to notify-send

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(gcmh))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook (lambda nil (org-babel-tangle)) nil
 	   t))))
